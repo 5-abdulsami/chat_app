a835f3fe4856aae0fb1221cfea1b2ff26463ee7d
5-abdulsami <5abdulsami2004@gmail.com>
2025-01-21 22:50:26 +0500

    using streambuilder to retrieve firestore data in app

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:linkup/api/api.dart';
 import 'package:linkup/main.dart';
 import 'package:linkup/widgets.dart/chat_user_card.dart';
 
@@ -23,12 +26,24 @@ class _HomeScreenState extends State<HomeScreen> {
           IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
         ],
       ),
-      body: ListView.builder(
-          padding: EdgeInsets.only(top: mediaQuery.height * 0.01),
-          itemCount: 10,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context, index) {
-            return ChatUserCard();
+      body: StreamBuilder(
+          stream: API.firestore.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            var list = [];
+            if (snapshot.hasData) {
+              final data = snapshot.data?.docs;
+              for (var i in data!) {
+                log('Data: ${i.data()}');
+                list.add(i.data()['name']);
+              }
+            }
+            return ListView.builder(
+                padding: EdgeInsets.only(top: mediaQuery.height * 0.01),
+                itemCount: list.length,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Text(list[index]);
+                });
           }),
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(10),
